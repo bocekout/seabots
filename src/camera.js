1bda823a583ea699956dd3a5ab23269afa0fafa5
Matt Bocek <mbocek@gmail.com>
2025-07-25 14:26:53 -0700

    expressions

--- src/camera.js
+++ src/camera.js
@@ -8,6 +8,9 @@ async function start() {
   await faceapi.nets.faceExpressionNet.loadFromUri('/models');
   await faceapi.nets.faceLandmark68TinyNet.loadFromUri('/models');
   await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
+  await faceapi.nets.ssdMobilenetv1.loadFromUri('/models');
+  await faceapi.nets.mtcnn.loadFromUri('/models');
+  await faceapi.nets.faceRecognitionNet.loadFromUri('/models');
 
   const stream = await navigator.mediaDevices.getUserMedia({ video: {} });
   video.srcObject = stream;
@@ -15,21 +18,29 @@ async function start() {
   video.addEventListener('play', () => {
     const canvas = faceapi.createCanvasFromMedia(video);
 
-    Object.assign(canvas.style, {position: 'absolute', top: 0, left: 0});
+    Object.assign(canvas.style, { position: 'absolute', top: 0, left: 0 });
     document.body.appendChild(canvas);
     const displaySize = { width: video.width, height: video.height };
     faceapi.matchDimensions(canvas, displaySize);
 
     setInterval(async () => {
-      const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks();
+      const detections = await faceapi.detectSingleFace(video, new faceapi.TinyFaceDetectorOptions())
+        .withFaceLandmarks()
+        .withAgeAndGender()
+        .withFaceDescriptor()
+        .withFaceExpressions();
 
-      //console.log(detections);
-      if (detections.length > 0) {
+
+      if (detections) {
         const resized = faceapi.resizeResults(detections, displaySize);
         canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
         faceapi.draw.drawDetections(canvas, resized);
-        //faceapi.draw.drawLandmarks(canvas, resized);
         faceapi.draw.drawFaceLandmarks(canvas, resized);
+        faceapi.draw.drawFaceExpressions(canvas, resized);
+
+        const faceMatcher = new faceapi.FaceMatcher(detections.descriptor);
+        const bestMatch = faceMatcher.findBestMatch(detections.descriptor);
+        console.log(bestMatch.toString());
       }
 
     }, 500);
