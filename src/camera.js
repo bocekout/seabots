41b1945723c5fb3c87e2e50f5d379767100afb1e
Matt Bocek <mbocek@gmail.com>
2025-07-25 12:51:50 -0700

    face detection with landmarks

--- src/camera.js
+++ src/camera.js
@@ -3,25 +3,36 @@ import * as faceapi from 'face-api.js';
 const video = document.getElementById('video');
 
 async function start() {
-   await faceapi.nets.tinyFaceDetector.loadFromUri('./models');
+  await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
+  await faceapi.nets.ageGenderNet.loadFromUri('/models');
+  await faceapi.nets.faceExpressionNet.loadFromUri('/models');
+  await faceapi.nets.faceLandmark68TinyNet.loadFromUri('/models');
+  await faceapi.nets.faceLandmark68Net.loadFromUri('/models');
 
   const stream = await navigator.mediaDevices.getUserMedia({ video: {} });
   video.srcObject = stream;
 
   video.addEventListener('play', () => {
     const canvas = faceapi.createCanvasFromMedia(video);
+
+    Object.assign(canvas.style, {position: 'absolute', top: 0, left: 0});
     document.body.appendChild(canvas);
     const displaySize = { width: video.width, height: video.height };
     faceapi.matchDimensions(canvas, displaySize);
 
     setInterval(async () => {
-      const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions());
-      const resized = faceapi.resizeResults(detections, displaySize);
-      canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
-      faceapi.draw.drawDetections(canvas, resized);
+      const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks();
 
-      console.log(detections);
-    }, 100);
+      //console.log(detections);
+      if (detections.length > 0) {
+        const resized = faceapi.resizeResults(detections, displaySize);
+        canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+        faceapi.draw.drawDetections(canvas, resized);
+        //faceapi.draw.drawLandmarks(canvas, resized);
+        faceapi.draw.drawFaceLandmarks(canvas, resized);
+      }
+
+    }, 500);
   });
 }
 
